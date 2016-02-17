be627b0e77ca5e377918fdcad09d8d02a0d95bab
Philippe Luchansky <philuchansky@gmail.com>
2016-02-16 23:02:49 -0800

    routes for new and create, controller actions for new and create, and private whitelisted params method in tweets controller.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,9 +8,16 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      redirect_to tweets_new_path
+    end
   end
 
   def edit
@@ -21,4 +28,9 @@ class TweetsController < ApplicationController
 
   def destroy
   end
+  
+  private
+    def tweet_params
+      params.require(:tweet).permit(:title, :body)
+    end
 end
